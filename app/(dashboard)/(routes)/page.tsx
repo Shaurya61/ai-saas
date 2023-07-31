64c6a3123cf4a795d6e0afb6fffe8ddedfb280b4
Shaurya <shauryanayyarviic30@gmail.com>
2023-08-01 02:05:32 +0530

    commit

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+
+const  DashboardPage =() => {
+  return (
+    <Button variant={"destructive"} size="lg">click me </Button>
+  )
+}
+export default DashboardPage;
