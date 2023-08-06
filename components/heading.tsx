eaa02b060d9ca1f35eb757ab9d4642a5ede7341a
Shaurya <shauryanayyarviic30@gmail.com>
2023-08-07 03:46:15 +0530

    commit

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 
 interface HeadingProps{
@@ -11,13 +12,23 @@ interface HeadingProps{
 export const Heading = ({
     title,
     description,
-    icon,
+    icon:   Icon,
     iconColor,
     bgColor,
 }: HeadingProps) => {
     return (
+        <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+            <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+                <Icon className={cn("w-10 h-10", iconColor)} />
+        </div>
         <div>
-            Heading Component
+        <h2 className="text-3xl font-bold">
+            {title}
+        </h2>
+        <p className="text-sm text-muted-foreground font-light text-sm md:text-lg">
+            {description}
+        </p>
+        </div>
         </div>
     );
 };
