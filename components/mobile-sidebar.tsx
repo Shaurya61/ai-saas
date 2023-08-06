48c160ac259962d0f109b07079befa121206722a
Shaurya <shauryanayyarviic30@gmail.com>
2023-08-07 03:02:11 +0530

    commit

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,29 @@
 "use client";
 import { Menu } from "lucide-react";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+import { useState,useEffect } from "react";
 const MobileSidebar = () => {
+
+    //fixing hydration error
+    const [ismounted,setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    if (!ismounted) return null;
+
   return (
-    <Button variant="ghost" size="icon" className="md:hidden">
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   );
 }
 
